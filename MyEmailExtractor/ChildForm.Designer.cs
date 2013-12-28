58c97b9060b1143f23bc0c0e83491ec69fb4978f
dprotopopov <protopopov@narod.ru>
2013-12-28 15:59:20 +0400

    13.2.5

--- MyEmailExtractor/ChildForm.Designer.cs
+++ MyEmailExtractor/ChildForm.Designer.cs
@@ -152,6 +152,7 @@
             // 
             // webBrowser1
             // 
+            this.webBrowser1.AllowNavigation = false;
             this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
             this.webBrowser1.Location = new System.Drawing.Point(0, 0);
